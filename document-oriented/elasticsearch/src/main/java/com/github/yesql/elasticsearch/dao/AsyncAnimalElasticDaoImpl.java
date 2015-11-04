8553f97ed2e3c2cfc8dbeefbda15e39cb495f895
Martin Janys <martinjanys@gmail.com>
2015-11-04 22:21:20 +0100

    feat(elastic) crud

--- document-oriented/elasticsearch/src/main/java/com/github/yesql/elasticsearch/dao/AsyncAnimalElasticDaoImpl.java
+++ document-oriented/elasticsearch/src/main/java/com/github/yesql/elasticsearch/dao/AsyncAnimalElasticDaoImpl.java
@@ -3,10 +3,13 @@ package com.github.yesql.elasticsearch.dao;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.yesql.elasticsearch.ActionThrowsListener;
 import com.github.yesql.elasticsearch.model.ElasticAnimal;
+import org.elasticsearch.action.ActionFuture;
+import org.elasticsearch.action.count.CountRequestBuilder;
 import org.elasticsearch.action.count.CountResponse;
 import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.Client;
@@ -16,10 +19,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.Assert;
 
 import java.io.IOException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  * @author Martin Janys
@@ -44,12 +52,11 @@ public class AsyncAnimalElasticDaoImpl extends AbstractElasticDao implements Asy
         client.prepareGet(INDEX, TYPE, id).execute(new ActionThrowsListener<GetResponse>() {
             @Override
             public void onResponse(GetResponse getFields) {
-                try {
-                    future.complete(toDto(getFields.getSourceAsString()));
-                }
-                catch (IOException e) {
-                    future.completeExceptionally(e);
-                }
+                future.complete(toDto(getFields.getSourceAsString()));
+            }
+            @Override
+            public void onFailure(Throwable e) {
+                future.completeExceptionally(e);
             }
         });
         return future;
@@ -58,44 +65,61 @@ public class AsyncAnimalElasticDaoImpl extends AbstractElasticDao implements Asy
     @Override
     public Future<List<ElasticAnimal>> findAllEntries() {
         CompletableFuture<List<ElasticAnimal>> future = new CompletableFuture<>();
-        client.prepareSearch(INDEX).setTypes(TYPE).setQuery(QueryBuilders.matchAllQuery()).execute(new ActionThrowsListener<SearchResponse>() {
+        finAllQuery().setQuery(QueryBuilders.matchAllQuery()).execute(new ActionThrowsListener<SearchResponse>() {
             @Override
             public void onResponse(SearchResponse searchResponse) {
                 List<ElasticAnimal> result = new ArrayList<>();
                 SearchHit[] hits = searchResponse.getHits().getHits();
-                try {
-                    for (SearchHit hit : hits) {
-                        result.add(toDto(hit.getSourceAsString()));
-                    }
-                    future.complete(result);
-                } catch (IOException e) {
-                    future.completeExceptionally(e);
+                for (SearchHit hit : hits) {
+                    ElasticAnimal elasticAnimal = toDto(hit.getSourceAsString());
+                    elasticAnimal.setId(hit.getId());
+                    result.add(elasticAnimal);
                 }
+                future.complete(result);
+            }
+            @Override
+            public void onFailure(Throwable e) {
+                future.completeExceptionally(e);
             }
         });
         return future;
     }
 
+    private SearchRequestBuilder finAllQuery() {
+        return client.prepareSearch(INDEX).setTypes(TYPE).setSize((int) countAllQuery().get().getCount());
+    }
+
     @Override
-    public Future<String> saveEntry(ElasticAnimal o) {
+    public Future<String> saveEntry(ElasticAnimal animal) {
+        Assert.isNull(animal.getId());
         final CompletableFuture<String> future = new CompletableFuture<>();
-        client.prepareIndex(INDEX, TYPE).execute(new ActionThrowsListener<IndexResponse>() {
+        client.prepareIndex(INDEX, TYPE).setSource(toJson(animal)).execute(new ActionThrowsListener<IndexResponse>() {
             @Override
             public void onResponse(IndexResponse indexResponse) {
+                animal.setId(indexResponse.getId());
                 future.complete(indexResponse.getId());
+            }
+
+            @Override
+            public void onFailure(Throwable e) {
+                future.completeExceptionally(e);
             }
         });
         return future;
     }
 
     @Override
-    public Future<String> updateEntry(ElasticAnimal o) {
-        Assert.notNull(o.getId());
+    public Future<String> updateEntry(ElasticAnimal animal) {
+        Assert.notNull(animal.getId());
         final CompletableFuture<String> future = new CompletableFuture<>();
-        client.prepareUpdate(INDEX, TYPE, o.getId()).execute(new ActionThrowsListener<UpdateResponse>() {
+        client.prepareUpdate(INDEX, TYPE, animal.getId()).setDoc(toJson(animal)).execute(new ActionThrowsListener<UpdateResponse>() {
             @Override
             public void onResponse(UpdateResponse updateResponse) {
                 future.complete(updateResponse.getId());
+            }
+            @Override
+            public void onFailure(Throwable e) {
+                future.completeExceptionally(e);
             }
         });
         return future;
@@ -108,6 +132,10 @@ public class AsyncAnimalElasticDaoImpl extends AbstractElasticDao implements Asy
             @Override
             public void onResponse(DeleteResponse deleteResponse) {
                 future.complete(deleteResponse.isFound());
+            }
+            @Override
+            public void onFailure(Throwable e) {
+                future.completeExceptionally(e);
             }
         });
         return future;
@@ -122,23 +150,41 @@ public class AsyncAnimalElasticDaoImpl extends AbstractElasticDao implements Asy
     @Override
     public Future<Integer> countAll() {
         CompletableFuture<Integer> future = new CompletableFuture<>();
-        client.prepareCount(INDEX).setTypes(TYPE).execute(new ActionThrowsListener<CountResponse>() {
+        countAllQuery().execute(new ActionThrowsListener<CountResponse>() {
             @Override
             public void onResponse(CountResponse countResponse) {
                 future.complete((int) countResponse.getCount());
+            }
+
+            @Override
+            public void onFailure(Throwable e) {
+                future.completeExceptionally(e);
             }
         });
         return future;
     }
 
+    private CountRequestBuilder countAllQuery() {
+        return client.prepareCount(INDEX).setTypes(TYPE).setQuery(QueryBuilders.matchAllQuery());
+    }
+
     @Override
     public Future<Boolean> deleteAll() {
         CompletableFuture<Boolean> future = new CompletableFuture<>();
-        client.prepareSearch(INDEX).execute(new ActionThrowsListener<SearchResponse>() {
+        finAllQuery().execute(new ActionThrowsListener<SearchResponse>() {
             @Override
             public void onResponse(SearchResponse searchResponse) {
-                searchResponse.getHits().forEach(searchHitFields -> client.prepareDelete(INDEX, TYPE, searchHitFields.getId()));
-                future.complete(true);
+                Boolean ret = Arrays.asList(searchResponse.getHits().getHits())
+                        .stream()
+                        .map(searchHitFields -> client.prepareDelete(INDEX, TYPE, searchHitFields.getId()).execute())
+                        .map(ActionFuture::actionGet)
+                        .allMatch(DeleteResponse::isFound);
+                future.complete(ret);
+            }
+
+            @Override
+            public void onFailure(Throwable e) {
+                future.completeExceptionally(e);
             }
         });
         return future;
@@ -184,7 +230,21 @@ public class AsyncAnimalElasticDaoImpl extends AbstractElasticDao implements Asy
         return null;
     }
 
-    private ElasticAnimal toDto(String sourceAsString) throws IOException {
-        return mapper.readValue(sourceAsString, ElasticAnimal.class);
+    private ElasticAnimal toDto(String sourceAsString) {
+        try {
+            return mapper.readValue(sourceAsString, ElasticAnimal.class);
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String toJson(ElasticAnimal value) {
+        try {
+            return mapper.writeValueAsString(value);
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- document-oriented/elasticsearch/src/test/java/com/github/yesql/elasticsearch/dao/AnimalElasticSpringDaoTest.java
+++ document-oriented/elasticsearch/src/test/java/com/github/yesql/elasticsearch/dao/AnimalElasticSpringDaoTest.java
@@ -1,9 +1,17 @@
 package com.github.yesql.elasticsearch.dao;
 
+import com.github.yesql.couchdb.model.Animal;
 import com.github.yesql.couchdb.test.AnimalDaoIntegrationTest;
 import com.github.yesql.elasticsearch.ElasticConfig;
 import com.github.yesql.elasticsearch.model.ElasticAnimal;
+import org.elasticsearch.client.AdminClient;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.List;
 
 import static org.testng.Assert.*;
 
@@ -15,7 +23,32 @@ import static org.testng.Assert.*;
 )
 public class AnimalElasticSpringDaoTest extends AnimalDaoIntegrationTest {
 
+    @Autowired
+    private AdminClient client;
+
     public AnimalElasticSpringDaoTest() {
         super(ElasticAnimal.class);
+    }
+
+    @BeforeMethod
+    public void refresh() {
+        client.indices().prepareFlush("animal").execute();
+        client.indices().prepareClearCache("animal").execute();
+        client.indices().prepareRefresh("animal").execute();
+    }
+
+    public void testDeleteAll() {
+        List<Animal> allEntries = dao.findAllEntries();
+        int count = dao.countAll();
+        refresh();
+        dao.deleteEntry(allEntries.get(0));
+
+        refresh();
+        assertEquals(dao.countAll(), count - 1);
+
+        dao.deleteAll();
+
+        refresh();
+        assertEquals(dao.countAll(), 0);
     }
 }
