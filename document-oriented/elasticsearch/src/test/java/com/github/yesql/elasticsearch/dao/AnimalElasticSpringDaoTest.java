d442dc28985c7e16e86c264346e374a65e767f06
Martin Janys <martinjanys@gmail.com>
2015-11-07 15:37:19 +0100

    ignore incomplete test

--- document-oriented/elasticsearch/src/test/java/com/github/yesql/elasticsearch/dao/AnimalElasticSpringDaoTest.java
+++ document-oriented/elasticsearch/src/test/java/com/github/yesql/elasticsearch/dao/AnimalElasticSpringDaoTest.java
@@ -24,6 +24,7 @@ import static org.testng.Assert.*;
 @ContextConfiguration(
         classes = ElasticConfig.class
 )
+@Test(enabled = false)
 public class AnimalElasticSpringDaoTest extends AnimalDaoIntegrationTest {
 
     @Autowired
